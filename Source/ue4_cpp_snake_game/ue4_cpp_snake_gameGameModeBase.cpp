a9377678f342d175f7187d4fb5e6b85e343bcbcf
lxx-ue <71316369+lxx-ue@users.noreply.github.com>
2022-07-07 18:24:13 +0300

    Initial commit

---
+++ Source/ue4_cpp_snake_game/ue4_cpp_snake_gameGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ue4_cpp_snake_gameGameModeBase.h"
+
---
+++ Source/ue4_cpp_snake_game/ue4_cpp_snake_gameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ue4_cpp_snake_gameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_CPP_SNAKE_GAME_API Aue4_cpp_snake_gameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
